1f22705cc2ff517bcc301d6f138da35e76af0706
beaukams <kamstelecom@hotmail.com>
2015-09-22 12:47:54 +0000

    ajout de appui/layouts/js

--- js/app.js
+++ js/app.js
@@ -12,45 +12,8 @@ require.def("vente/js/app",
 			init: function(){ 
 			var self = this; 
 			this._super("vente"); 
-			//on cree les boutons et on ajoute a chaque bouton une Label 
-			var jaune = new Button("jaune"); 
-			jaune.appendChildWidget(new Label("Jaune")); 
-			var bleu = new Button("bleu"); 
-			bleu.appendChildWidget(new Label("Bleu")); 
-			var rouge = new Button("rouge"); 
-			rouge.appendChildWidget(new Label("Rouge")); 
-		 
-			//conteneur qui va afficher la couleur 
-			var comp = new Container("comp"); 
-			//changement de couleurs 
-			jaune.addEventListener("select", function(evt){ 
-				comp.addClass("jaune"); 
-				comp.removeClass("bleu"); 
-				comp.removeClass("rouge"); 
-			}); 
-			 bleu.addEventListener("select", function(evt){ 
-                                comp.addClass("bleu"); 
-                                comp.removeClass("jaune"); 
-                                comp.removeClass("rouge"); 
-                        }); 
-			 rouge.addEventListener("select", function(evt){ 
-                                comp.addClass("rouge"); 
-                                comp.removeClass("bleu"); 
-                                comp.removeClass("jaune"); 
-                        }); 
 
-			//creer une liste vertical et y ajouter les boutons 
-			var menu = new VerticalList("verticallist"); 
-			menu.appendChildWidget(jaune); 
-			menu.appendChildWidget(bleu); 
-			menu.appendChildWidget(rouge); 
-			 
-			//disposition horizontal 
-			var horizontal = new HorizontalList("horizontallist"); 
-			horizontal.appendChildWidget(menu); 
-			horizontal.appendChildWidget(comp); 
-	 
-			this.appendChildWidget(horizontal); 
+			
 			} 
 		}); 
 	} 
